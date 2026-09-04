dda2af4ec625946e262db63a59e7743761efff42
Nguyen-Phi-Long-702 <nguyenphilong722006@gmail.com>
2026-09-04 20:36:09 +0700

    chore: thêm dependency dagger hilt, room, retrofit, navigation, lifecycle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -29,9 +30,12 @@ android {
             )
         }
     }
+    buildFeatures {
+        buildConfig = true
+    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
@@ -41,4 +45,26 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //Lifecycle-viewmodel & livedata
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.9.8")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.9.8")
+
+    //Jetpack navigation component
+    implementation("androidx.navigation:navigation-fragment:2.9.8")
+    implementation("androidx.navigation:navigation-ui:2.9.8")
+
+    //Dagger hilt
+    implementation("com.google.dagger:hilt-android:2.60.1")
+    annotationProcessor("com.google.dagger:hilt-compiler:2.60.1")
+
+    //Room
+    implementation("androidx.room:room-runtime:2.8.4")
+    annotationProcessor("androidx.room:room-compiler:2.8.4")
+
+    //Retrofit(gọi supabase auth rest api)
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
 }
