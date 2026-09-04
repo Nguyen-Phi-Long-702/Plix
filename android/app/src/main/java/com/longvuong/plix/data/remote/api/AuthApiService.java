fa8aa1f47d63b45a507c06fd4b1113297dc8fb4f
Nguyen-Phi-Long-702 <nguyenphilong722006@gmail.com>
2026-09-04 21:04:54 +0700

    feat: thêm AuthApiService goi supabase auth rest api

---
+++ android/app/src/main/java/com/longvuong/plix/data/remote/api/AuthApiService.java
@@ -0,0 +1,22 @@
+package com.longvuong.plix.data.remote.api;
+
+import com.longvuong.plix.data.remote.dto.AuthResponseDto;
+import com.longvuong.plix.data.remote.dto.LoginRequestDto;
+import com.longvuong.plix.data.remote.dto.SignupRequestDto;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface AuthApiService {
+
+    @POST("auth/v1/signup")
+    Call<AuthResponseDto> signup(@Body SignupRequestDto body);
+
+    @POST("auth/v1/token")
+    Call<AuthResponseDto> login(
+            @Query("grant_type") String grantType, 
+            @Body LoginRequestDto body
+    );
+}
