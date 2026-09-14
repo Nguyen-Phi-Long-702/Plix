2a628da9a1e1a26a8e5901f553effe65256cfefc
Nguyen-Phi-Long-702 <nguyenphilong722006@gmail.com>
2026-09-14 11:59:43 +0700

    feat: Gắn màn hình quản lí danh mục vào tab cài đặt

---
+++ android/app/src/main/java/com/longvuong/plix/presentation/settings/SettingsFragment.java
@@ -0,0 +1,34 @@
+package com.longvuong.plix.presentation.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.longvuong.plix.R;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class SettingsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        View rowCategoryManagement = view.findViewById(R.id.rowCategoryManagement);
+        rowCategoryManagement.setOnClickListener(v ->
+                Navigation.findNavController(view)
+                        .navigate(R.id.action_settingsFragment_to_categoryManagementFragment));
+    }
+}
